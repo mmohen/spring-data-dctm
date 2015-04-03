ba285073acab54d4b3aeac1b35ff287da38e8eb5
RamanWalia <ramanw@gmail.com>
2015-04-03 22:34:58 +0530

    setContent and getContent implemented.

--- src/main/java/com/emc/documentum/springdata/core/Application.java
+++ src/main/java/com/emc/documentum/springdata/core/Application.java
@@ -3,7 +3,6 @@ package com.emc.documentum.springdata.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 import com.documentum.fc.common.DfException;
 
